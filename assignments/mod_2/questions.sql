c50ce2d51aa3ce65223387286635c6c13df8ea90
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-07-07 12:01:49 -0400

    finished up module 2 assignment

--- assignments/mod_2/questions.sql
+++ assignments/mod_2/questions.sql
@@ -10,3 +10,34 @@ WHERE
 
 -- 3. Which attribute could be a primary key for the Employee table?
 -- A: Employee_ID
+
+-- 4. How many decimal places (maximum) can be stored in an employee’s salary field?
+-- A: A maximum of two decimal places can be stored.
+
+-- 5. How many decimal places (maximum) can a Department_ID have?
+-- A: Department_ID is a SMALLINT therefore it will not have any decimal places.
+
+-- 6. What three rules do tables obey?
+-- A: 1. Unique primary key
+--    2. Unique column names
+--    3. No duplicate rows
+
+-- 7. Is the Employee table normalized? How do you know?
+/*
+    A: Yes, the Employee table is normalized becouse it meets the
+       definiton of First Normal Form (1NF),
+
+    First Normal Form (1NF)
+    - All rows must be unique.
+    - Each cell must contain a single value.
+    - Each value should be non divisible (can't be split down further)
+*/
+
+-- 8. What is the result of the following query? 
+SELECT SUM(Salary) FROM Employee WHERE Department_ID = 3;
+
+-- 9. Identify the Employee ID(s) that would be returned as a result of the following query. 
+SELECT * FROM Employee WHERE Classification <> 'Exempt';
+
+-- 10. What is the result of the following query? 
+SELECT MAX(Salary) FROM Employee;
