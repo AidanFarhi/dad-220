62a736e42a3309d1faea37e38b5e3354522acd63
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-07-09 18:36:42 -0400

    added mysql_basics dir and select lesson sql

---
+++ mysql_basics/select.sql
@@ -0,0 +1,18 @@
+SELECT lastName FROM employees;
+
+SELECT lastName, firstName, jobTitle
+FROM employees;
+
+SELECT * FROM employees;
+
+SELECT 1 + 1;
+
+SELECT NOW();
+
+SELECT CONCAT('John', ' ', 'Doe');
+
+SELECT NOW() FROM dual;
+
+SELECT CONCAT('John', ' ', 'Doe') AS name;
+
+SELECT CONCAT('John', ' ', 'Doe') AS 'Full name';
